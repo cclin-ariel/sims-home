7423705c413202e81c7bbead14a1e30db5800de8
cclin <71698978+cclin-ariel@users.noreply.github.com>
2021-12-06 22:38:58 +0900

    Initial commit

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- component -->
+  <Header />
+
+  <!-- <div class="flex w-full h-screen">
+    <SideBar />
+    <section class="w-full p-4">
+      <div class="w-full h-64 border-dashed border-4 p-4 text-md">
+        Dashboard
+      </div>
+    </section>
+  </div> -->
+</template>
+<script>
+import Header from "./Header.vue";
+import SideBar from "./SideBar.vue";
+export default {
+  components: {
+    Header,
+    SideBar,
+  },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/pages/Login.vue'
+import DashBoard from '../components/DashBoard.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '*',
+    redirect: 'login' // path
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/admin',
+    name: 'DashBoard',
+    component: DashBoard,
+    // children: [
+    //   {
+    //     path: 'products',
+    //     name: 'products',
+    //     component: Products,
+    //     meta: { requiresAuth: true } // router info
+    //   },
+    //   {
+    //     path: 'order_list',
+    //     name: 'orderList',
+    //     component: OrderList,
+    //     meta: { requiresAuth: true } // router info
+    //   },
+    //   {
+    //     path: 'coupon_list',
+    //     name: 'couponList',
+    //     component: CouponList,
+    //     meta: { requiresAuth: true } // router info
+    //   }
+    // ]
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
