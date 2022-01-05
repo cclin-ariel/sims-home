30da307d5e40689c055138b3ec026bbb20421be4
cclin <71698978+cclin-ariel@users.noreply.github.com>
2022-01-05 23:48:26 +0900

    Shop nav layout

---
+++ src/components/customer/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-screen flex flex-col items-center">
+    <ShopNav/>
+    <!-- section fo INSPIRATION DELIVERED -->
+    <ShopFooter/>
+  </div>
+</template>
+<script>
+import ShopNav from './ShopNav.vue'
+import ShopFooter from './ShopFooter.vue'
+export default {
+    components:{ShopNav, ShopFooter}
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,81 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../components/pages/Login.vue'
-import Products from '../components/pages/Products.vue'
-import OrderList from '../components/pages/OrderList.vue'
-import CouponList from '../components/pages/CouponList.vue'
-import DashBoard from '../components/DashBoard.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/pages/Login.vue";
+import Products from "../components/pages/Products.vue";
+import OrderList from "../components/pages/OrderList.vue";
+import CouponList from "../components/pages/CouponList.vue";
+import DashBoard from "../components/DashBoard.vue";
 
-Vue.use(VueRouter)
+// customer page
+import Home from "../components/customer/Home.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '*',
-    redirect: 'login' // path
+    path: "*",
+    redirect: "login", // path
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/admin',
-    name: 'DashBoard',
+    path: "/admin",
+    name: "DashBoard",
     component: DashBoard,
     children: [
       {
-        path: 'products',
-        name: 'products',
+        path: "products",
+        name: "products",
         component: Products,
-        meta: { requiresAuth: true } // router info
+        meta: { requiresAuth: true }, // router info
       },
       {
-        path: 'order_list',
-        name: 'orderList',
+        path: "order_list",
+        name: "orderList",
         component: OrderList,
-        meta: { requiresAuth: true } // router info
+        meta: { requiresAuth: true }, // router info
       },
       {
-        path: 'coupon_list',
-        name: 'couponList',
+        path: "coupon_list",
+        name: "couponList",
         component: CouponList,
-        meta: { requiresAuth: true } // router info
-      }
-    ]
+        meta: { requiresAuth: true }, // router info
+      },
+    ],
   },
-]
+
+  // customer order
+
+  // {
+  //   path: '/',
+  //   name: 'Dashboard',
+  //   component: Dashboard,
+  //   children: [
+  //     {
+  //       path: 'customer_orders',
+  //       name: 'customerOrders',
+  //       component: CustomerOrders
+  //     },
+  //     {
+  //       path: 'customer_checkout/:orderId',
+  //       name: 'customerCheckout',
+  //       component: CustomerCheckout
+  //     }
+  //   ]
+  // },
+  { 
+    path: '/sims_home', 
+    name: 'Home', 
+    component: Home }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
