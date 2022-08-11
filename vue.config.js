2065dcc6576b46a65694fb2a1d52abda436f7e95
cclin <71698978+cclin-ariel@users.noreply.github.com>
2022-08-11 17:46:05 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // publicPath 為 project-name，根目錄地址為上傳的網域
     publicPath: '/sims-home/',
-    devServer: {
-        https: true
-    }
+    // devServer: {
+    //     https: true
+    // }
 }
